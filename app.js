df85fd86f84446226f3b1709984c5278f36593a0
derrick-long <long.derrick8@gmail.com>
2018-03-09 22:05:48 -0500

    add routes

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// index route
+app.get('/', (req, res)=>{
+  res.send('INDEX');
+});
+
+// about route
+
+app.get('/about', (req, res)=>{
+  res.send('ABOUT 1');
+});
 
 const port = 5000;
 
