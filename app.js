4423654073cc6502eab8e6a2c24cbbcbada7adbf
derrick-long <long.derrick8@gmail.com>
2018-03-10 19:53:24 -0500

    add mongoose and connect db

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const mongoose = require('mongoose');
+
+
+// connect to mongoose
+mongoose.connect('mongodb://localhost/video-notes',{
+ 
+})
+.then(()=>console.log('MongoDB Connected...'))
+.catch(err=> console.log(err));
+
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
