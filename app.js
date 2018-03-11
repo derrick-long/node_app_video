034c46be69de31acf6c8810ff2f52545c393e8fb
derrick-long <long.derrick8@gmail.com>
2018-03-10 21:59:32 -0500

    add new routes and idea form

--- app.js
+++ app.js
@@ -37,6 +37,14 @@ app.get('/about', (req, res)=>{
   res.render('about');
 });
 
+// idea form
+
+
+app.get('/ideas/add', (req, res)=>{
+  res.render('ideas/add');
+});
+
+
 const port = 5000;
 
 app.listen(port,()=>{
