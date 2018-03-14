e216d17589c8bb64176f1fda7f2931c7420c363b
derrick-long <long.derrick8@gmail.com>
2018-03-14 12:53:24 -0400

    add simple email validation

--- routes/users.js
+++ routes/users.js
@@ -38,27 +38,35 @@ router.post('/register', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    bcrypt.genSalt(10, (err, salt)=>{
-      bcrypt.hash(newUser.password, salt, (err, hash)=> {
-        if(err) throw err;
-        newUser.password = hash;
-        newUser.save()
-          .then(user => {
-            req.flash('success_msg', 'Registration complete!');
-            res.redirect('/users/login');
-          })
-          .catch(err=>{
-            console.log(err);
-            return;
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          req.flash('error_msg', 'Email already regsitered');
+          res.redirect('/users/register');
+        } else {
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
           });
-       });
-    });
 
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => {
+                  req.flash('succes_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                });
+            });
+          });
+        }
+      });
   }
 });
 
